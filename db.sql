5b0156c429db1af7d18ae148992405e9952ae7ab
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2013-11-02 18:38:29 +0530

    |#0|KJ|Added welcome home page and database config

---
+++ db.sql
@@ -0,0 +1,5 @@
+create role myapp_development with createdb login password 'password1';
+create role myapp_test with createdb login password 'password1';
+create role myapp_production with createdb login password 'password1';
+create role myapp with createdb login password 'password1';
+
